61f2a42d2f9c5ea8b4782cea5b46751aa8a6338a
perezrau <perezrau@PEREZRAU2>
2018-12-12 22:43:40 -0600

    Add project files.

---
+++ DailyMeals1/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using WSDailyMeals.Algorithm;
+
+namespace DailyMeals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double totalKCal = double.Parse(args[0]);
+
+            double KCalProt = totalKCal * .15;
+            double KCalCarbs = totalKCal * .55;
+            double KCalLipid = totalKCal * .3;
+
+            Core c = new Core(KCalLipid, KCalCarbs, KCalProt);
+            string result = JsonConvert.SerializeObject(c.result);
+            Console.Write(result);
+        }
+    }
+}
